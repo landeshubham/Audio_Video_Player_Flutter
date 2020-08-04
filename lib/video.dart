2b9d4bf50407eedd53315fa549992700d36e6e77
landeshubham <55054432+landeshubham@users.noreply.github.com>
2020-08-05 01:59:46 +0530

    Update video.dart

--- lib/video.dart
+++ lib/video.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:iqplayer/iqplayer.dart';
-//import 'package:http/http.dart';
 
 class MyVideo extends StatelessWidget {
   @override
